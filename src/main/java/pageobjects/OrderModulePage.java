b784ae713841bc9a38a18fff663798fd6f021438
Екатерина <ekaterina@MacBook-Pro-Ekaterina.local>
2022-01-20 07:55:35 +0300

    Fix for Sales Organization field failing by this.wait()

--- src/main/java/pageobjects/OrderModulePage.java
+++ src/main/java/pageobjects/OrderModulePage.java
@@ -101,6 +101,15 @@ public class OrderModulePage {
         $x(String.format("//*[@title='%s']", orderDTO.getPriceBook())).shouldBe(visible, Duration.ofSeconds(3000)).click();
 
         SALESORGFIELD.sendKeys(orderDTO.getSalesOrganization());
+        try {
+            //https://overcoder.net/q/855235/%D0%BF%D0%BE%D1%87%D0%B5%D0%BC%D1%83-java-%D0%B2%D1%8B%D0%B1%D1%80%D0%B0%D1%81%D1%8B%D0%B2%D0%B0%D0%B5%D1%82-javalangillegalmonitorstateexception-%D0%BA%D0%BE%D0%B3%D0%B4%D0%B0-%D1%8F-%D0%B2%D1%8B%D0%B7%D1%8B%D0%B2%D0%B0%D1%8E
+            synchronized(this){
+                this.wait(1000);
+            }
+            System.out.println("======== wait 1000 millis ========");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         if(COMPLETETHISFIELDMESSAGE.isDisplayed()){
             SALESORGFIELD.click();
         }
