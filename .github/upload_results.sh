a0fe96a647c8bd086457a773605fbe22f0a31719
alexeypaletsky <e73ecd38efb245c24de501de0d8c677ebc238746>
2022-02-10 18:58:41 +0600

    change region

--- .github/upload_results.sh
+++ .github/upload_results.sh
@@ -7,5 +7,5 @@ then
 version="fb-"$version
 fi
 aws s3 sync $1 "s3://ctwebreports/$version"
-echo "::warning::See report: http://ctwebreports.s3-website-us-east-1.amazonaws.com/$version/allureReport/index.html"
-echo "REPORT_URL=http://ctwebreports.s3-website-us-east-1.amazonaws.com/$version/allureReport/index.html" >> $GITHUB_ENV
+echo "::warning::See report: http://ctwebreports.s3-website-eu-west-1.amazonaws.com/$version/allureReport/index.html"
+echo "REPORT_URL=http://ctwebreports.s3-website-eu-west-1.amazonaws.com/$version/allureReport/index.html" >> $GITHUB_ENV
